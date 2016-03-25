5a0f697fbc88d784f35c0d7fe45c6a3f13556730
PFlorians <patrikflorians@gmail.com>
2016-03-25 13:03:14 +0100

    Update PathFinder.java

--- PathFinder.java
+++ PathFinder.java
@@ -6,30 +6,43 @@ public class PathFinder {
 	private Stack stk;
 	private ArrayList<Policko> cesta;
 	private int uzolminx=99999, uzolminy=99999;
+	private List<Integer> list=new ArrayList<Integer>();
+	
 	public PathFinder()
 	{
 		this.cesta=new ArrayList<Policko>();
+		this.list.add(0);
+		this.list.add(1);
+		this.list.add(2);
+		this.list.add(3);
+		this.list.add(4);
+		this.list.add(5);
+		this.list.add(6);
+		this.list.add(7);
 	}
-	private void mv1(int sx, int sy, int mx, int my, Policko chessboard[][])
+	private void mv0(int sx, int sy, int mx, int my, Policko chessboard[][])
 	{
-		
-	}
-	public boolean next(int sx, int sy, int mx, int my, Policko chessboard[][]) throws Exception
-	{
-		this.uzolminx=99999;
-		this.uzolminy=99999;//nijako nesuvisi s found
-		System.out.println("idem z: " + sy + " " + sx);
-		
 		if((sy+2>=0 && sy+2<=my) && (sx+1>=0 && sx+1<=mx))
 		{
 		//	System.out.println("kontrola: " + (sy+2) + " " + (sx+1) + " my/mx " + my + " " +mx);
 			if((chessboard[sy+2][sx+1].getFound()==99999 || chessboard[sy+2][sx+1].getFound()==-1))
 			{
-				if(sx+1<uzolminx || sy+2<uzolminy)
+				if((sy+2<my/2 || sx+1<mx/2) && (sy+2<this.uzolminy || sx+1<this.uzolminx))
 				{
-					uzolminy=sy+2;
-					uzolminx=sx+1;
+					this.uzolminy=sy+2;
+					this.uzolminx=sx+1;
 		//			System.out.println("kontrola2.0: " + uzolminy + " " + uzolminx + " my/mx " + my + " " +mx);
+				}
+				else if((sy+2>my/2 || sx+1>mx/2) && (sy+2>this.uzolminy || sx+1>this.uzolminx))
+				{
+					this.uzolminy=sy+2;
+					this.uzolminx=sx+1;
+				}
+				else if((sy+2==my/2 || sx+1==mx/2) &&
+						((sy+2<this.uzolminy || sx+1<this.uzolminx) || (sy+2>this.uzolminy || sx+1>this.uzolminx)))
+				{
+					this.uzolminy=sy+2;
+					this.uzolminx=sx+1;
 				}
 				if(chessboard[sy+2][sx+1].getFound()>0)
 				{
@@ -37,16 +50,30 @@ public class PathFinder {
 				}
 			}
 		}
+	}
+	private void mv1(int sx, int sy, int mx, int my, Policko chessboard[][])
+	{
 		if((sy+2>=0 && sy+2<=my) && (sx-1>=0 && sx-1<=mx))
 		{
 		//	System.out.println("kontrola: " + (sy+2) + " " + (sx-1) + " my/mx " + my + " " +mx);
 			if(chessboard[sy+2][sx-1].getFound()==99999 || chessboard[sy+2][sx-1].getFound()==-1)
 			{
-				if(sx-1<uzolminx || sy+2<uzolminy)
+				if((sy+2>my/2 || sx-1>mx/2 ) && (sy+2<this.uzolminy || sx-1<this.uzolminx))
 				{
-					uzolminy=sy+2;
-					uzolminx=sx-1;
+					this.uzolminy=sy+2;
+					this.uzolminx=sx-1;
 		//			System.out.println("kontrola2.1: " + uzolminy + " " + uzolminx + " my/mx " + my + " " +mx);
+				}
+				else if((sy+2>my/2 || sx-1>mx/2 ) && (sy+2>this.uzolminy || sx-1>this.uzolminx))
+				{
+					this.uzolminy=sy+2;
+					this.uzolminx=sx-1;
+				}
+				else if((sy+2==my/2 || sx-1==mx/2 ) && 
+					((sy+2<this.uzolminy || sx-1<this.uzolminx) || (sy+2>this.uzolminy || sx-1>this.uzolminx)))
+				{
+					this.uzolminy=sy+2;
+					this.uzolminx=sx-1;
 				}
 				if(chessboard[sy+2][sx-1].getFound()>0)
 				{
@@ -54,16 +81,30 @@ public class PathFinder {
 				}
 			}
 		}
+	}
+	private void mv2(int sx, int sy, int mx, int my, Policko chessboard[][])
+	{
 		if((sy+1>=0 && sy+1<=my) && (sx+2>=0 && sx+2<=mx))
 		{
 		//	System.out.println("kontrola: " + (sy+1) + " " + (sx+2) + " my/mx " + my + " " +mx);
 			if(chessboard[sy+1][sx+2].getFound()==99999 || chessboard[sy+1][sx+2].getFound()==-1)
 			{
-				if(sx+2<uzolminx || sy+1<uzolminy)
+				if((sy+1<my/2 || sx+2<mx/2 ) && (sy+1<this.uzolminy || sx+2<this.uzolminx))
 				{
-					uzolminy=sy+1;
-					uzolminx=sx+2;
+					this.uzolminy=sy+1;
+					this.uzolminx=sx+2;
 			//		System.out.println("kontrola2.2: " + uzolminy + " " + uzolminx + " my/mx " + my + " " +mx);
+				}
+				else if((sy+1>my/2 || sx+2>mx/2 ) && (sy+1>this.uzolminy || sx+2>this.uzolminx))
+				{
+					this.uzolminy=sy+1;
+					this.uzolminx=sx+2;
+				}
+				else if((sy+1==my/2 || sx+2==mx/2 ) &&
+					((sy+1<this.uzolminy || sx+2<this.uzolminx) || (sy+1>this.uzolminy || sx+2>this.uzolminx)))
+				{
+					this.uzolminy=sy+1;
+					this.uzolminx=sx+2;
 				}
 				if(chessboard[sy+1][sx+2].getFound()>0)
 				{
@@ -71,16 +112,30 @@ public class PathFinder {
 				}
 			}
 		}
+	}
+	private void mv3(int sx, int sy, int mx, int my, Policko chessboard[][])
+	{
 		if((sy+1>=0 && sy+1<=my) && (sx-2>=0 && sx-2<=mx))
 		{
 		//	System.out.println("kontrola: " + (sy+1) + " " + (sx-2) + " my/mx " + my + " " +mx);
 			if(chessboard[sy+1][sx-2].getFound()==99999 || chessboard[sy+1][sx-2].getFound()==-1)
 			{
-				if(sx-2<uzolminx || sy+1<uzolminy)
+				if((sy+1<my/2 || sx-2<mx/2 ) && (sy+1<this.uzolminy || sx-2<this.uzolminx))
 				{
-					uzolminy=sy+1;
-					uzolminx=sx-2;
+					this.uzolminy=sy+1;
+					this.uzolminx=sx-2;
 			//		System.out.println("kontrola2.3: " + uzolminy + " " + uzolminx + " my/mx " + my + " " +mx);
+				}
+				else if((sy+1>my/2 || sx-2>mx/2 ) && (sy+1>this.uzolminy || sx-2>this.uzolminx))
+				{
+					this.uzolminy=sy+1;
+					this.uzolminx=sx-2;
+				}
+				else if((sy+1==my/2 || sx-2==mx/2 ) &&
+					((sy+1<this.uzolminy || sx-2<this.uzolminx) || (sy+1>this.uzolminy || sx-2>this.uzolminx)))
+				{
+					this.uzolminy=sy+1;
+					this.uzolminx=sx-2;
 				}
 				if(chessboard[sy+1][sx-2].getFound()>0)
 				{
@@ -88,16 +143,30 @@ public class PathFinder {
 				}
 			}
 		}
+	}
+	private void mv4(int sx, int sy, int mx, int my, Policko chessboard[][])
+	{
 		if((sy-1>=0 && sy-1<=my) && (sx+2>=0 && sx+2<=mx))
 		{
 			//System.out.println("kontrola: " + (sy-1) + " " + (sx+2) + " my/mx " + my + " " +mx);
 			if(chessboard[sy-1][sx+2].getFound()==99999 || chessboard[sy-1][sx+2].getFound()==-1)
 			{
-				if(sx+2<uzolminx || sy-1<uzolminy)
+				if((sy-1<my/2 || sx+2<mx/2 ) && (sy-1<this.uzolminy || sx+2<this.uzolminx))
 				{
-					uzolminy=sy-1;
-					uzolminx=sx+2;
+					this.uzolminy=sy-1;
+					this.uzolminx=sx+2;
 				//	System.out.println("kontrola2.4: " + uzolminy + " " + uzolminx + " my/mx " + my + " " +mx);
+				}
+				else if((sy-1>my/2 || sx+2>mx/2 ) && (sy-1>this.uzolminy || sx+2>this.uzolminx))
+				{
+					this.uzolminy=sy-1;
+					this.uzolminx=sx+2;
+				}
+				else if((sy-1==my/2 || sx+2==mx/2 ) && 
+					((sy-1<this.uzolminy || sx+2<this.uzolminx) || (sy-1>this.uzolminy || sx+2>this.uzolminx)))
+				{
+					this.uzolminy=sy-1;
+					this.uzolminx=sx+2;
 				}
 				if(chessboard[sy-1][sx+2].getFound()>0)
 				{
@@ -105,16 +174,30 @@ public class PathFinder {
 				}
 			}
 		}
+	}
+	private void mv5(int sx, int sy, int mx, int my, Policko chessboard[][])
+	{
 		if((sy-1>=0 && sy-1<=my) && (sx-2>=0 && sx-2<=mx))
 		{
 			//System.out.println("kontrola: " + (sy-1) + " " + (sx-2) + " my/mx " + my + " " +mx);
 			if(chessboard[sy-1][sx-2].getFound()==99999 || chessboard[sy-1][sx-2].getFound()==-1)
 			{
-				if(sx-2<uzolminx || sy-1<uzolminy)
+				if((sy-1>my/2 || sx-2<mx/2 ) && (sy-1<this.uzolminy || sx-2<this.uzolminx))
 				{
-					uzolminy=sy-1;
-					uzolminx=sx-2;
+					this.uzolminy=sy-1;
+					this.uzolminx=sx-2;
 					//System.out.println("kontrola2.5: " + uzolminy + " " + uzolminx + " my/mx " + my + " " +mx);
+				}
+				else if((sy-1>my/2 || sx-2>mx/2 ) && (sy-1<this.uzolminy || sx-2<this.uzolminx))
+				{
+					this.uzolminy=sy-1;
+					this.uzolminx=sx-2;
+				}
+				else if((sy-1==my/2 || sx-2==mx/2 ) &&
+					((sy-1<this.uzolminy || sx-2<this.uzolminx) || (sy-1<this.uzolminy || sx-2<this.uzolminx)))
+				{
+					this.uzolminy=sy-1;
+					this.uzolminx=sx-2;
 				}
 				if(chessboard[sy-1][sx-2].getFound()>0)
 				{
@@ -122,16 +205,30 @@ public class PathFinder {
 				}
 			}
 		}
+	}
+	private void mv6(int sx, int sy, int mx, int my, Policko chessboard[][])
+	{
 		if((sy-2>=0 && sy-2<=my) && (sx+1>=0 && sx+1<=mx))
 		{
 		//	System.out.println("kontrola: " + (sy-2) + " " + (sx+1) + " my/mx " + my + " " +mx);
 			if(chessboard[sy-2][sx+1].getFound()==99999 || chessboard[sy-2][sx+1].getFound()==-1)
 			{
-				if(sx+1<uzolminx || sy-2<uzolminy)
+				if((sy-2<my/2 || sx+1<mx/2 ) && (sy-2<this.uzolminy || sx+1<this.uzolminx))
 				{
-					uzolminy=sy-2;
-					uzolminx=sx+1;
+					this.uzolminy=sy-2;
+					this.uzolminx=sx+1;
 		//			System.out.println("kontrola2.6: " + uzolminy + " " + uzolminx + " my/mx " + my + " " +mx);
+				}
+				else if((sy-2>my/2 || sx+1>mx/2 ) && (sy-2<this.uzolminy || sx+1<this.uzolminx))
+				{
+					this.uzolminy=sy-2;
+					this.uzolminx=sx+1;
+				}
+				else if((sy-2==my/2 || sx+1==mx/2 ) && 
+					((sy-2<this.uzolminy || sx+1<this.uzolminx) || (sy-2<this.uzolminy || sx+1<this.uzolminx)))
+				{
+					this.uzolminy=sy-2;
+					this.uzolminx=sx+1;
 				}
 				if(chessboard[sy-2][sx+1].getFound()>0)
 				{
@@ -139,16 +236,30 @@ public class PathFinder {
 				}
 			}
 		}
+	}
+	private void mv7(int sx, int sy, int mx, int my, Policko chessboard[][])
+	{
 		if((sy-2>=0 && sy-2<=my) && (sx-1>=0 && sx-1<=mx))
 		{
 		//	System.out.println("kontrola: " + (sy-2) + " " + (sx-1) + " my/mx " + my + " " +mx);
 			if(chessboard[sy-2][sx-1].getFound()==99999 || chessboard[sy-2][sx-1].getFound()==-1)
 			{
-				if(sx-1<uzolminx || sy-2<uzolminy)
+				if((sy-2<my/2 || sx-1<mx/2 ) && (sy-2<this.uzolminy || sx-1<this.uzolminx))
 				{
-					uzolminy=sy-2;
-					uzolminx=sx-1;
+					this.uzolminy=sy-2;
+					this.uzolminx=sx-1;
 		//			System.out.println("kontrola2.7: " + uzolminy + " " + uzolminx + " my/mx " + my + " " +mx);
+				}
+				else  if((sy-2>my/2 || sx-1>mx/2 ) && (sy-2<this.uzolminy || sx-1<this.uzolminx))
+				{
+					this.uzolminy=sy-2;
+					this.uzolminx=sx-1;
+				}
+				else if((sy-2==my/2 || sx-1==mx/2 ) && 
+					((sy-2<this.uzolminy || sx-1<this.uzolminx) || (sy-2<this.uzolminy || sx-1<this.uzolminx)))
+				{
+					this.uzolminy=sy-2;
+					this.uzolminx=sx-1;
 				}
 				if(chessboard[sy-2][sx-1].getFound()>0)
 				{
@@ -156,32 +267,83 @@ public class PathFinder {
 				}
 			}
 		}
-		if(uzolminy!=99999 && uzolminx!=99999)
+	}
+	public boolean next(int sx, int sy, int mx, int my, Policko chessboard[][]) throws Exception
+	{
+		this.uzolminx=99999;
+		this.uzolminy=99999;//nijako nesuvisi s found
+		System.out.println("idem z: " + sy + " " + sx);
+		int i;
+		for(i=0;i<this.list.size();i++)
 		{
-			System.out.println("kam idem: " + uzolminy + " " + uzolminx);
-			chessboard[uzolminy][uzolminx].setFound(0);
+			if(this.list.get(i)==0)
+			{
+				mv0(sx, sy, mx, my, chessboard);
+			}
+			else if(this.list.get(i)==1)
+			{
+				mv1(sx, sy, mx, my, chessboard);
+			}
+			else if(this.list.get(i)==2)
+			{
+				mv2(sx, sy, mx, my, chessboard);
+			}
+			else if(this.list.get(i)==3)
+			{
+				mv3(sx, sy, mx, my, chessboard);
+			}
+			else if(this.list.get(i)==4)
+			{
+				mv4(sx, sy, mx, my, chessboard);
+			}
+			else if(this.list.get(i)==5)
+			{
+				mv5(sx, sy, mx, my, chessboard);
+			}
+			else if(this.list.get(i)==6)
+			{
+				mv6(sx, sy, mx, my, chessboard);
+			}
+			else if(this.list.get(i)==7)
+			{
+				mv7(sx, sy, mx, my, chessboard);
+			}
+			else
+			{
+				System.out.println("error");
+				break;
+			}
+		}
+		if(this.uzolminy!=99999 && this.uzolminx!=99999)
+		{
+			System.out.println("kam idem: " + this.uzolminy + " " + this.uzolminx);
+			chessboard[this.uzolminy][this.uzolminx].setFound(0);
 			
-			chessboard[uzolminy][uzolminx].setX(uzolminx);
-			chessboard[uzolminy][uzolminx].setY(uzolminy);
-			this.cesta.add(chessboard[uzolminy][uzolminx]);
+			chessboard[this.uzolminy][this.uzolminx].setX(this.uzolminx);
+			chessboard[this.uzolminy][this.uzolminx].setY(this.uzolminy);
+			this.cesta.add(chessboard[this.uzolminy][this.uzolminx]);
 			
-			this.stk=this.stk.push(chessboard[uzolminy][uzolminx]);
-			if(next(uzolminx, uzolminy, mx, my, chessboard)==false)
+			this.stk=this.stk.push(chessboard[this.uzolminy][this.uzolminx]);
+			while(next(this.uzolminx, this.uzolminy, mx, my, chessboard)==false 
+					&& this.cesta.size()>0)
 			{
 				this.cesta.remove(this.cesta.size()-1);
 				this.stk.dump();
 				this.stk=this.stk.pop();
-				uzolminx=this.stk.getTop().getX();
-				uzolminy=this.stk.getTop().getY();
+				this.uzolminx=this.stk.getTop().getX();
+				this.uzolminy=this.stk.getTop().getY();
 				//vyber dalsieho, pretoze podmienky hovoria len ak ==99999 alebo ==-1
-				next(uzolminx, uzolminy, mx, my, chessboard);
-				chessboard[uzolminy][uzolminx].setFound(-1);//mozem prist odinakial
+				next(this.uzolminx, this.uzolminy, mx, my, chessboard);
+				chessboard[this.uzolminy][this.uzolminx].setFound(-1);//mozem prist odinakial
 			}
 			return true;
 		}
 		else
 		{
-			
+			this.uzolminx=this.stk.getTop().getX();
+			this.uzolminy=this.stk.getTop().getY();
+			System.out.println("vraciam false: " + this.uzolminy + " " + this.uzolminx);
+			Collections.shuffle(this.list);
 			return false;
 		}
 	}
@@ -214,7 +376,15 @@ public class PathFinder {
 		max--;
 		try
 		{
-			next(0, 0, max, max, chessboard);
+			//while(this.cesta.size()<25)
+			//{
+				Collections.shuffle(this.list);
+				next(0, 0, max, max, chessboard);
+				if(this.cesta.size()<25)
+				{
+					System.out.println("path not found");
+				}
+			//}
 		}
 		catch(Exception e)
 		{
